ac76cd7c3bcfcdcb5060fc739fc540448e9e4982
sandun7787 <sandunwarnasooreya345@gmail.com>
2023-12-16 14:21:43 +0530

    use the new gym shop

--- src/App.js
+++ src/App.js
@@ -7,29 +7,35 @@ import { BrowserRouter, Form, Route } from 'react-router-dom';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import About from './Pages/About';
 import Content from './Components/Content';
+import { useState } from 'react';
     
 
 
 
 function App() {
+
+  const[count,setCount]=useState(0);
+
+  
+  const increment =() =>(
+    //count+=2
+    setCount(count+1)
+  );
+
+  const decrement =() =>(
+    //count-=2
+    setCount(count-1)
+  );
   return (
-  <Router>
-    <div>
-      <Switch>
-        <Route  exact path ="/">
-          <home/>
-        </Route>
+    <>
+    <span className='title'>
+      my country
+    </span>
+    <p className='subTitle'>the count is {count}</p>
+    <button onClick={decrement} className='button'>+</button>
+    <button onClick={increment} className='buttpon'>-</button>
+    </>
 
-        <Route path="/About">
-          <About/>
-        </Route>
-
-        <Route path="/Content">
-          <Content/>
-        </Route>
-      </Switch>
-    </div>
-  </Router>
   );
 }
 
