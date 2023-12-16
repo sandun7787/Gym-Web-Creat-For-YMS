503070b0ca9c0820b27fe834766a5c3ff817389c
sandun7787 <sandunwarnasooreya345@gmail.com>
2023-12-16 08:12:12 +0530

    add new component

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopicBox from '../Components/TopicBox'
+import Content from '../Components/Content'
+
+export default function Home (){
+    return(
+        <div>
+            <h1>this is the home page</h1>
+            <TopicBox/>
+            <Content/>
+        </div>
+    )
+    
+}
